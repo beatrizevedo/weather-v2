5fc739f495e45957b5d5d234c8e7b45d69f31b98
Beatriz Azevedo <bia.azevedo@live.com.pt>
2024-05-16 11:27:11 +0100

    Added api integration

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
+function weatherInfo(response) {
+  let temperature = document.querySelector('#current-temperature');
+  temperature.innerHTML = Math.round(response.data.temperature.current);
+  let weatherCity = document.querySelector('#weather-city');
+  weatherCity.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = '052t6596f0376314513oae46624edf8b';
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(weatherInfo);
+}
+
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector('#input-search-form');
-  let weatherCity = document.querySelector('#weather-city');
-  weatherCity.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector('#search-form');
 searchForm.addEventListener('submit', searchSubmit);
+
+searchCity('Porto');
