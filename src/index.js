3dbad31593c1fce88562493356dd957ea0545d51
Beatriz Azevedo <bia.azevedo@live.com.pt>
2024-05-21 14:06:57 +0100

    Finishing project

--- src/index.js
+++ src/index.js
@@ -26,6 +26,8 @@ function weatherInfo(response) {
 
   let weatherIcon = document.querySelector('#weather-icon');
   weatherIcon.innerHTML = `<img src=${response.data.condition.icon_url} />`;
+
+  getForecast(response.data.city);
 }
 
 function searchCity(city) {
@@ -38,6 +40,59 @@ function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector('#input-search-form');
   searchCity(searchInput.value);
+}
+
+function getForecast(city) {
+  apiKey = '052t6596f0376314513oae46624edf8b';
+  apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecastHTML = '';
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `<div> 
+      <img src="${
+        day.condition.icon_url
+      }" id="forecast-weather-icon" class="forecast-weather-icon"/>
+    
+        <div class="forecast-weather-temperature" id="forecast-weather-temperature">
+        <span class="forecast-min-temperature" id="forecast-min-temperature">${Math.round(
+          day.temperature.minimum
+        )}°</span>
+        <span class="forecast-max-temperature" id="forecast-max-temperature">${Math.round(
+          day.temperature.maximum
+        )}°</span>
+        </div>
+        <hr />
+        <div class="forecast-weather-weekday" id="forecast-weather-weekday">${formatDate(
+          day.time
+        )}</div>
+        </div>`;
+    }
+  });
+
+  function formatDate(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thrusday',
+      'Friday',
+      'Saturday',
+    ];
+
+    return days[date.getDay()];
+  }
+
+  let forecast = document.querySelector('#forecast');
+  forecast.innerHTML = forecastHTML;
 }
 
 let searchForm = document.querySelector('#search-form');
